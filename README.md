2f43989282d67969682c284d1f67754ab5331f7f
chris-gillatt <solex@hotmail.co.uk>
2022-09-15 20:58:22 +0100

    Add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# [![CI](https://github.com/chris-gillatt/gamez-joke-bot/actions/workflows/main.yml/badge.svg)](https://github.com/chris-gillatt/gamez-joke-bot/actions/workflows/main.yml) gamez-joke-bot (Betty)
+
+Joke bot to post to the Gamez Discord server. 
+
+Posts a joke every day at 20:00 UTC. 
---
+++ send-joke.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# A script to send a joke to gamez Discord channel.  Intended to be triggered by cron.
+
+post_joke () {
+  curl -s -H 'Content-Type: application/json' --data "{\"content\" : \"$PRELUDE \n$JOKE $SIGN_OFF \"}" "$WEBHOOK" > /dev/null
+}
+
+PRELUDE="but first, a joke... \n"
+JOKE=$(curl -s https://icanhazdadjoke.com)
+WEBHOOK="$BETTY_WEBHOOK"
+SIGN_OFF="\n\nmmmmhahahahAHAHAHA \n\n   ENOUGH!"
+
+if [ -z "$JOKE" ]; then
+  PRELUDE="I'm bleeding which makes me the victor."
+  JOKE="I rock.  And roll.  All night long.  Sweet suzy."
+  WEBHOOK="$WIMPLO_WEBHOOK"
+  SIGN_OFF=""
+fi
+
+post_joke
+
